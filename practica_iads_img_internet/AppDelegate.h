eb08252f953c7216e45803e365e11b5c468023c9
Rogelio Torres <rogeliotoba@gmail.com>
2014-03-13 20:47:46 -0600

    Initial Commit

---
+++ practica_iads_img_internet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  practica_iads_img_internet
+//
+//  Created by Rogelio Torres on 13/03/14.
+//  Copyright (c) 2014 Rogelio Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ practica_iads_img_internetTests/practica_iads_img_internetTests.m
@@ -0,0 +1,34 @@
+//
+//  practica_iads_img_internetTests.m
+//  practica_iads_img_internetTests
+//
+//  Created by Rogelio Torres on 13/03/14.
+//  Copyright (c) 2014 Rogelio Torres. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface practica_iads_img_internetTests : XCTestCase
+
+@end
+
+@implementation practica_iads_img_internetTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
